10dc14a3f7ded25c324e7bd0cb5a782e8ae4f710
Rayreid76 <rayreid76@gmail.com>
2018-08-08 14:05:59 -0700

    collections

---
+++ language_essentials/Arrays/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring an array of length 5, initialized by default to all zeroes
+            int[] numArray = new int[5];
+            // Declaring an array with pre-populated values
+            // For Arrays initialized this way, the length is determined by the size of the given data
+            int[] numArray2 = {1,2,3,4,6};
+            // It is possible to declare an array without initialization, but you must use the "new" operator once you do define the array's values
+            int[] array3;
+            array3 = new int[] {1,3,5,7,9};
+            Console.WriteLine(numArray[0]);
+            Console.WriteLine(numArray2[0]);
+            Console.WriteLine(array3[0]);
+            Console.WriteLine("*********");
+
+            //accessing arrays
+            int[] arrayOfInts = {1, 2, 3, 4, 5};
+            Console.WriteLine(arrayOfInts[0]);    // The first number lives at index 0.
+            Console.WriteLine(arrayOfInts[1]);    // The second number lives at index 1.
+            Console.WriteLine(arrayOfInts[2]);    // The third number lives at index 2.
+            Console.WriteLine(arrayOfInts[3]);    // The fourth number lives at index 3.
+            Console.WriteLine(arrayOfInts[4]);    // The fifth and final number lives at index 4.
+            Console.WriteLine("*********");
+
+            int[] arr = {1, 2, 3, 4};
+            Console.WriteLine("The first number of the arr is " + arr[0]); 
+            arr[0] = 8;
+            Console.WriteLine("The first number of the arr is now " + arr[0]);   // The array has now changed!
+            Console.WriteLine("*********");
+
+            // iterating through an array
+            string[] myCars = new string[4] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"};
+            // The 'Length' property tells us how many values are in the Array (4 in our example here). copy
+            // We can use this to determine where the loop ends: Length-1 is the index of the last value. 
+            for (int idx = 0; idx < myCars.Length; idx++)
+            {
+                Console.WriteLine("I own a {0}", myCars[idx]);
+            }
+            Console.WriteLine("*********");
+
+            // Foreach
+            //string[] myCars = new string[4] { "Mazda Miata", "Ford Model T", "Dodge Challenger", "Nissan 300zx"}; 
+            foreach (string car in myCars)
+            {
+                // We no longer need the index, because variable 'car' already contains each indexed value
+                Console.WriteLine("I own a {0}", car);
+            }
+
+
+        }
+    }
+}
---
+++ schoolwork/Collections/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array to hold int 0 to 9
+            int[] num = {0,1,2,3,4,5,6,7,8,9};
+            // array to hold names
+            string[] name = {"Tim", "Martin", "Nikki", "Sara"};
+            // array of bool
+            string[] TFarray = {"True", "False", "True", "False", "True", "False", "True", "False", "True", "False"};
+
+            // list of ice cream flavors
+            List<string> icecream = new List<string>();
+            icecream.Add("Mint Chip");
+            icecream.Add("Rocky Road");
+            icecream.Add("Salted Carmel");
+            icecream.Add("Cookie n Cream");
+            icecream.Add("Cherry");
+            icecream.Add("Neopalatin");
+            icecream.Add("Vanilla");
+            icecream.Add("Chocolate");
+            icecream.Add("Pistachio");
+            icecream.Add("Moose Tracks");
+
+            // Lenght of array
+            Console.WriteLine("We have {0} ice creams in our list.", icecream.Count);
+            Console.WriteLine("The third ice cream in list: {0}",icecream[2]);
+            icecream.Remove("Salted Carmel");
+            Console.WriteLine("We now have {0} ice cream in out list.", icecream.Count);
+
+            // Dictionary
+            Dictionary<string,string> Fav = new Dictionary<string,string>();
+            Fav.Add("Tim", "Rocky Road");
+            Fav.Add("Martin", "Vanilla");
+            Fav.Add("Nikki", "Pistachio");
+            Fav.Add("Sara", "Moose Tracks");
+            Console.WriteLine("Dictionary:");
+            foreach (KeyValuePair<string,string> entry in Fav)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+            
+            {
+                
+            }
+
+        }
+    }
+}
